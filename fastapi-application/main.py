ea8ae5fce912e5102c8fe7c4d37b64372fb42564
vans1r <serebrogml@gmail.com>
2026-03-18 21:53:08 +0300

    import settings

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
+from api import router as api_router
+from core.config import settings
 
 app = FastAPI()
-
-
-def main():
-    pass
-
+app.include_router(
+    api_router,
+    prefix=settings.api.prefix,
+)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8001)
+    uvicorn.run("main:app", reload=True, host=settings.run.host, port=settings.run.port, )
