e46a372dffcf86fb8479d6adfd7356186fec320d
vans1r <serebrogml@gmail.com>
2026-03-18 21:52:32 +0300

    create core.config.py file with app settings

---
+++ fastapi-application/core/config.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8001
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+
+class Settings(BaseSettings):
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+
+
+
+settings = Settings()
