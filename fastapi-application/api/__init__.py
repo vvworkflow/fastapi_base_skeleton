3c128db46e99519c22ec2f336080633f3fe09c0e
vans1r <serebrogml@gmail.com>
2026-03-20 01:53:08 +0300

    connect api_v1_router to api_router

--- fastapi-application/api/__init__.py
+++ fastapi-application/api/__init__.py
@@ -1,4 +1,11 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+from core.config import settings
+from .api_v1 import router as api_v1_router
 
+router = APIRouter(
+    prefix=settings.api.prefix,
+)
+router.include_router(
+    api_v1_router,
+)
