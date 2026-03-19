d9f43a8dd4ee2098cb7b3af4b1783ac59483e845
vans1r <serebrogml@gmail.com>
2026-03-20 00:37:59 +0300

    add naming_conventions for alembic migrations

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -19,14 +19,21 @@ class DatabaseConfig(BaseModel):
     max_overflow: int = 50
     pool_size: int = 10
 
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         case_sensitive=False,
-        env_file=["../.env.example", "../.env"],
+        env_file=(".env.example", ".env"),
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
-
     )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -1,8 +1,13 @@
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import MetaData
+
+from core.config import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
