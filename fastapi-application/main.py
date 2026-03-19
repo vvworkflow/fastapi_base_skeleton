fcdea258296947fbc135d6ce67a21a25b8526385
vans1r <serebrogml@gmail.com>
2026-03-19 22:45:53 +0300

    create lifespan

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,13 +1,30 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 import uvicorn
 from api import router as api_router
 from core.config import settings
+from core.models import db_helper
 
-app = FastAPI()
-app.include_router(
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
+main_app.include_router(
     api_router,
     prefix=settings.api.prefix,
 )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, host=settings.run.host, port=settings.run.port, )
+    uvicorn.run(
+        "main:main_app",
+        reload=True,
+        host=settings.run.host,
+        port=settings.run.port,
+    )
